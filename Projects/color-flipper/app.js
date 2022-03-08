fcd77314836a8ac58c66c18ed304f4c239d85fa5
em_wilson <emilyellenwilson@gmail.com>
2022-03-07 21:46:02 -0500

    Continued with Color Flipper project

--- Projects/color-flipper/app.js
+++ Projects/color-flipper/app.js
@@ -1,5 +1,5 @@
 //Set list of colors for basic flip
-const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const colors = ["green", "red", "rgba(133,122,200)", "#f15025", "pink", "yellow", "lightblue"];
 
 //Grab the button element
 const btn = document.getElementById("btn");
@@ -7,4 +7,18 @@ const btn = document.getElementById("btn");
 
 //Grab the span element with class of color
 const color = document.querySelector(".color"); 
-console.log(color); 
+// console.log(color); 
+
+//Add event listener for clicking on the button
+btn.addEventListener("click", function(){
+    //get random number between 0-3 to pull from an index position in the colors array
+    const randomNumber = getRandomNumber();
+    console.log(randomNumber); 
+    document.body.style.backgroundColor = colors[randomNumber]; //Target the background color of the entire body and change it to green when button is clicked.
+    color.textContent = colors[randomNumber]; //Grab the text inside colors span. Replace with text in the index position of the colors array. 
+});
+
+//Create function to generate a true random number
+function getRandomNumber() {
+    return Math.floor(Math.random() * colors.length); //Multiply the random # (0 - 0.99) by length of the array and then round down to match an actual index position
+}
--- Projects/color-flipper/hex.js
+++ Projects/color-flipper/hex.js
@@ -1,1 +1,13 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+//Grab button element
+const btn = document.querySelector(".btn"); 
+// console.log(btn); 
+
+//Grab span element with class of color
+const color = document.querySelector(".color");
+// console.log(color);  
+
+btn.addEventListener("click", function(){
+
+})
