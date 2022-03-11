40f635561d73cea63b5f67d1b125af368c532195
em_wilson <emilyellenwilson@gmail.com>
2022-03-11 18:34:14 -0500

    Working on faq page adding html, css and js

--- Projects/faq-page/app.js
+++ Projects/faq-page/app.js
@@ -1,2 +1,16 @@
-//Method Two: Using Selectors Inside The Element
 // Method One: Traversing the DOM
+const btn = document.querySelector(".question-btn");
+console.log(btn); 
+
+const answerText = document.querySelector(".answer"); 
+console.log(answerText); 
+
+btn.addEventListener("click", function(){
+    answerText.classList.add="show-text"; 
+
+})
+
+
+
+
+//Method Two: Using Selectors Inside The Element
