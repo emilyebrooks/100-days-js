d9393c38236e78b0e95f89ee2644bc8e70c74a2a
em_wilson <emilyellenwilson@gmail.com>
2022-03-15 07:51:06 -0400

    faq page practice

--- Projects/faq-page/app.js
+++ Projects/faq-page/app.js
@@ -1,17 +1,17 @@
 // Method One: Traversing the DOM
 const btns = document.querySelectorAll(".question-btn");
-// console.log(btns); 
-
-const answerText = document.querySelector(".answer"); 
-// console.log(answerText); 
+console.log(btns); 
 
 btns.forEach(function (btn) {
     btn.addEventListener("click", function (e) {
-        console.log(e.this); 
+        //Select <article class="question">, not just the button
+        //This applies the .show-text styles to the entire container, not just the button
+        //console.log(e.currentTarget.parentElement.parentElement); 
+        const question = e.currentTarget.parentElement.parentElement; 
+        question.classList.toggle("show-text"); 
+        //toggle() toggles between hide and show
     });
 });
-
-
 
 
 //Method Two: Using Selectors Inside The Element
