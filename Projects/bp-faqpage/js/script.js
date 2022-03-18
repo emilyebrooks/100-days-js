46a14506fab7d5bd64fead905bb8733bfaab2f2a
em_wilson <emilyellenwilson@gmail.com>
2022-03-18 19:23:01 -0400

    JS added to create expandable Q&A list

--- Projects/bp-faqpage/js/script.js
+++ Projects/bp-faqpage/js/script.js
@@ -7,3 +7,15 @@ function toggleMenu() {
     barMenu.style.display = "block";
   }
 }
+
+const btns = document.querySelectorAll(".qa-button"); 
+// console.log(btns); 
+
+
+btns.forEach(function (btn) {
+  btn.addEventListener("click", function(e) {
+    const toggleAnswer = e.currentTarget.parentElement.parentElement; 
+    console.log(toggleAnswer); 
+    toggleAnswer.classList.toggle("show-answer"); 
+  });
+});
